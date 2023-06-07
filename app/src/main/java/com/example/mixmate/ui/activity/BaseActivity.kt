9cca8c96dd47a4968997cc55d5f1c12ec906bc65
Rahmat Afriyanton <rahmatafriyanton@gmail.com>
2023-06-07 11:04:06 +0700

    Add camera page

--- app/src/main/java/com/example/mixmate/ui/activity/BaseActivity.kt
+++ app/src/main/java/com/example/mixmate/ui/activity/BaseActivity.kt
@@ -1,12 +1,54 @@
 package com.example.mixmate.ui.activity
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.mixmate.R
+import com.example.mixmate.databinding.ActivityBaseBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BaseActivity : AppCompatActivity() {
+  private lateinit var bottomNavigationView: BottomNavigationView
+
+  private val onNavigationItemSelectedListener =
+    BottomNavigationView.OnNavigationItemSelectedListener { item ->
+      when (item.itemId) {
+        R.id.nav_home -> {
+          showFragment(HomeFragment())
+          return@OnNavigationItemSelectedListener true
+        }
+        R.id.nav_camera -> {
+          showFragment(CameraFragment())
+          return@OnNavigationItemSelectedListener true
+        }
+        R.id.nav_recommendation -> {
+          showFragment(RecommendationFragment())
+          return@OnNavigationItemSelectedListener true
+        }
+        R.id.nav_profile -> {
+          showFragment(ProfileFragment())
+          return@OnNavigationItemSelectedListener true
+        }
+      }
+      false
+    }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_base)
+
+    bottomNavigationView = findViewById(R.id.bottomNavigationView)
+    bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+    // Set default fragment
+    showFragment(HomeFragment())
+  }
+
+  private fun showFragment(fragment: Fragment) {
+    supportFragmentManager.beginTransaction()
+      .replace(R.id.fragmentContainer, fragment)
+      .commit()
   }
 }
---
+++ app/src/main/java/com/example/mixmate/ui/activity/CameraFragment.kt
@@ -0,0 +1,115 @@
+package com.example.mixmate.ui.activity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.mixmate.databinding.FragmentCameraBinding
+
+class CameraFragment : Fragment() {
+
+  private var _binding: FragmentCameraBinding? = null
+  private val binding get() = _binding!!
+
+  private var cameraProvider: ProcessCameraProvider? = null
+  private var camera: Camera? = null
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentCameraBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding.buttonSubmit.setOnClickListener {
+      // Handle submit button click
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    requestCameraPermission()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    releaseCamera()
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+
+  private fun requestCameraPermission() {
+    if (ContextCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.CAMERA
+      ) == PackageManager.PERMISSION_GRANTED
+    ) {
+      startCamera()
+    } else {
+      ActivityCompat.requestPermissions(
+        requireActivity(),
+        arrayOf(Manifest.permission.CAMERA),
+        REQUEST_CAMERA_PERMISSION
+      )
+    }
+  }
+
+  private fun startCamera() {
+    val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+    cameraProviderFuture.addListener({
+      cameraProvider = cameraProviderFuture.get()
+      bindCameraUseCases()
+    }, ContextCompat.getMainExecutor(requireContext()))
+  }
+
+  private fun bindCameraUseCases() {
+    val cameraSelector = CameraSelector.Builder()
+      .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+      .build()
+
+    val preview = Preview.Builder().build().also {
+      it.setSurfaceProvider(binding.previewView.surfaceProvider)
+    }
+
+    try {
+      cameraProvider?.unbindAll()
+
+      camera = cameraProvider?.bindToLifecycle(
+        this,
+        cameraSelector,
+        preview
+      )
+    } catch (e: Exception) {
+      Log.e(TAG, "Failed to bind camera use cases: ${e.message}")
+    }
+  }
+
+  private fun releaseCamera() {
+    cameraProvider?.unbindAll()
+    cameraProvider = null
+    camera = null
+  }
+
+  companion object {
+    private const val TAG = "CameraFragment"
+    private const val REQUEST_CAMERA_PERMISSION = 123
+  }
+}
+
+
